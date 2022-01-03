caaef6931ab5be8f3062db45a6ad7f731cbf385a
duanjiajun <961329177@qq.com>
2022-01-03 19:38:12 +0800

    首次提交

---
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -0,0 +1,20 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
+    //会查询出多条讨论记录，动态sql有时候要拼接userId，有时候不用。
+    // offset起始行号，limit最多显示多少条
+
+    // @Param注解用于给参数取别名,
+    // 如果只有一个参数,并且在<if>里使用,则必须加别名.
+    int selectDiscussPostRows(@Param("userId") int userId);
+    //查询帖子的个数
+}
